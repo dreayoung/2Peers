4a0439b804ba999a5993b811f9da04b1e0a1e6ec
dreayoung <38860105+dreayoung@users.noreply.github.com>
2021-05-12 14:58:08 -0400

    Feat/footer (#37)
    * backend frontend connection
    
    * footer component
    
    Co-authored-by: Itzel01 <itzel.areymi.rendon@gmail.com>

--- 2Peers-Backend/index.js
+++ 2Peers-Backend/index.js
@@ -9,5 +9,10 @@ app.listen(PORT, () => {
 });
 
 app.get('/api', (req, res) => {
+<<<<<<< HEAD
+    res.send({ message: "Heyyyyyyyyy" });
+  });
+=======
   res.send({ message: 'Heyyyyyyyyy' });
 });
+>>>>>>> main
