59d6ab8dd104d051b2562b5ae93a008f7e2f644e
dreayoung <youngmandrea@gmail.com>
2021-05-13 14:39:20 -0400

    404page changes

--- 2Peers-Backend/index.jsx
+++ 2Peers-Backend/index.jsx
@@ -1,13 +1,11 @@
 const express = require('express');
-
 const PORT = process.env.PORT || 8000;
-
 const app = express();
-
-app.listen(PORT, () => {
-  console.log(`Server listening on ${PORT}`);
-});
 
 app.get('/api', (req, res) => {
   res.send({ message: 'Heyyyyyyyyy' });
+});
+
+app.listen(PORT, () => {
+  console.log(`Server listening on ${PORT}`);
 });
