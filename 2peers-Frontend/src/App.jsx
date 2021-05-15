bf22c3e31a532def47a856035561028ef53fb752
jason <jasonalvarezcoraiza@gmail.com>
2021-05-15 12:11:13 -0400

    Revert "center info of home doneish, need to replace images"
    This reverts commit 34b054344bcc2b4ae46339cb739456cb94741924.

--- 2peers-Frontend/src/App.jsx
+++ 2peers-Frontend/src/App.jsx
@@ -3,7 +3,6 @@ import { Route, Switch } from 'react-router-dom';
 import Login from './data/Login';
 import Signup from './data/Signup';
 import Page404 from './data/Page404';
-import Home from './Home/Home';
 // import Message from './Classroom/Message';
 // import Navbar from './Reusable/Navbar';
 // import Banner from './Reusable/Banner';
@@ -14,7 +13,6 @@ function App() {
       <Switch>
         <Route exact path="/login" component={Login} />
         <Route exact path="/signup" component={Signup} />
-        <Route exact path="/" component={Home} />
         <Route path="/" component={Page404} />
       </Switch>
     </div>
