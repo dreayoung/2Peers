baf4aeb4fa5329cfaf795a72424d9b28bf237e56
Jason Alvarez <jasonalvarezcoraiza@gmail.com>
2021-05-15 15:20:34 -0400

    Feat/class create frontend (#43)
    Can create classes through the front end
    
    Co-authored-by: Itzel01 <itzel.areymi.rendon@gmail.com>

--- 2peers-Frontend/src/App.jsx
+++ 2peers-Frontend/src/App.jsx
@@ -3,6 +3,7 @@ import { Route, Switch } from 'react-router-dom';
 import Login from './data/Login';
 import Signup from './data/Signup';
 import Page404 from './data/Page404';
+import Teacher from './Teacher/Teacher';
 // import Message from './Classroom/Message';
 // import Navbar from './Reusable/Navbar';
 // import Banner from './Reusable/Banner';
@@ -13,6 +14,7 @@ function App() {
       <Switch>
         <Route exact path="/login" component={Login} />
         <Route exact path="/signup" component={Signup} />
+        <Route exact path="/teachers/:id" component={Teacher} />
         <Route path="/" component={Page404} />
       </Switch>
     </div>
--- 2peers-Frontend/src/Teacher/Teacher.jsx
+++ 2peers-Frontend/src/Teacher/Teacher.jsx
@@ -1,13 +1,16 @@
 import React from 'react';
+// import MakeClass from './MakeClass';
 import Profile from '../Reusable/Profile';
 import Classlist from '../Reusable/Classlist';
 
 export default function Teacher() {
+  // make the modol pop up when a button is pressed functionality still in process....
   return (
     <div className="grid justify-items-center">
       <h1>teacher page</h1>
       <Profile />
       <Classlist />
+      {/* <MakeClass /> */}
     </div>
   );
 }
